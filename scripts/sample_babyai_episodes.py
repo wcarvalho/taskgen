93530bc7f8328f4e24ccd2066f12e20f4f98a6ce
Wilka Carvalho <wcarvalho92@gmail.com>
2021-01-20 11:31:46 -0500

    minor

--- scripts/sample_babyai_episodes.py
+++ scripts/sample_babyai_episodes.py
@@ -6,19 +6,32 @@ def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--level', help='BabyAI level', default='GoToLocal')
     parser.add_argument('--num-missions', help='# of unique missions', default=10)
-    parser.add_argument('--num-distractors', type=int, default=2)
-    parser.add_argument('--room-size', type=int, default=12)
+    parser.add_argument('--num-distractors', type=int, default=0)
+    parser.add_argument('--room-size', type=int, default=8)
+    parser.add_argument('--num-rows', type=int, default=0)
+    parser.add_argument('--steps', type=int, default=1)
     args = parser.parse_args()
 
     env_class = getattr(iclr19_levels, "Level_%s" % args.level)
-    env = env_class(room_size=args.room_size, num_dists=args.num_distractors)
 
+    kwargs={}
+    if args.num_distractors:
+        kwargs['num_dists'] = args.num_distractors
+
+    if args.num_rows:
+        kwargs['num_rows'] = args.num_rows
+        kwargs['num_cols'] = args.num_rows
+    env = env_class(room_size=args.room_size, **kwargs)
+    env.render('human')
 
     for mission_indx in range(args.num_missions):
         env.seed(mission_indx)
         obs = env.reset()
         print(obs['mission'])
-        env.render()
+        # action=1; env.step(action); env.render()
+        for step in range(args.steps):
+            obs, _, _, _ = env.step(env.action_space.sample())
+            env.render('human')
         ipdb.set_trace()
 
 if __name__ == "__main__":
