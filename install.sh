1e19c40c8a94b80bd71abcb2eda4247785517081
wilka <wcarvalho92@gmail.com>
2021-01-25 01:39:33 -0500

    baby gen scripts now maps tasks. fixed script for generating tasks/vocab for kitchen

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
-git clone https://github.com/wcarvalho/gym-minigrid.git gym-minigrid
-cd gym-minigrid
+git clone https://github.com/wcarvalho/gym-minigrid.git _gym-minigrid
+cd _gym-minigrid
 git checkout wilka
 pip install --editable .
 cd ..
 
-git clone https://github.com/wcarvalho/babyai.git babyai
-cd babyai
+git clone https://github.com/wcarvalho/babyai.git _babyai
+cd _babyai
 git checkout wilka
 pip install --editable .
 cd ..
 
-git clone https://github.com/wcarvalho/rlpyt rlpyt
-cd rlpyt
+git clone https://github.com/wcarvalho/rlpyt _rlpyt
+cd _rlpyt
 git checkout wilka
 pip install --editable .
 cd ..
--- setup.sh
+++ setup.sh
@@ -3,5 +3,6 @@ conda env create -f gpu.yaml
 source activate sfgen
 
 bash install.sh
+bash setup_lab.sh
 
 # conda remove --name sfgen --all
