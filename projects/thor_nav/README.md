959477cc7fc149c10f96b69599926094061fcaa3
wilka <wcarvalho92@gmail.com>
2021-10-15 23:34:46 -0400

    improved readability

--- projects/thor_nav/README.md
+++ projects/thor_nav/README.md
@@ -1,5 +1,25 @@
+#Install
 
 ```bash
 
-
+bash projects/thor_nav/setup.sh gpu
+bash projects/thor_nav/setup.sh cpu
 ```
+
+# Running
+
+```bash
+# setup env
+export PYTHONPATH="${PYTHONPATH}:." # otherwise doesn't recognize sfgen
+# startup x-server
+xinit&
+
+# run stuff
+python projects/thor_nav/launch_individual.py        # single experiment
+python projects/thor_nav/launch_batch.py                      # batched experiments in parallel
+```
+
+# Editing Runs
+
+- `individual_log.py` specifies latest settings to run for `launch_individual.py`
+- `batch_log.py` specifies latest search to run for `launch_batch.py`
--- projects/thor_nav/individual_log.py
+++ projects/thor_nav/individual_log.py
@@ -1,8 +1,11 @@
+# ======================================================
+# Local Testing
+# ======================================================
 config=dict(
     env=dict(verbosity=1),
     runner=dict(
-        n_steps=5e7, # 1e6=1 million, 1e8=100 million
-        log_interval_steps=1e4,
+        n_steps=100e3, # 100k
+        log_interval_steps=1e3, # 1K
     ),
     algo=dict(),
     sampler=dict(
@@ -10,3 +13,35 @@ config=dict(
             eval_n_envs=1,
             )
 )
+
+# ======================================================
+# Server, Testing
+# ======================================================
+config=dict(
+    env=dict(verbosity=0),
+    runner=dict(
+        n_steps=100e3, # 100k
+        log_interval_steps=1e3, # 1K
+    ),
+    algo=dict(),
+    sampler=dict(
+            batch_B=10,
+            eval_n_envs=2,
+            )
+)
+
+# # ======================================================
+# # Server, Single Run
+# # ======================================================
+# config=dict(
+#     env=dict(verbosity=0),
+#     runner=dict(
+#         n_steps=5e7, # 1e6=1 million, 1e8=100 million
+#         log_interval_steps=50e3, # 100K
+#     ),
+#     algo=dict(),
+#     sampler=dict(
+#             batch_B=10,
+#             eval_n_envs=2,
+#             )
+# )
